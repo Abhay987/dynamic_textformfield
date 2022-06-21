80d879bd8b81d081688da37abdd146c08e4954b0
himanshu yadav <himanshu@techtonic.asia>
2022-06-21 10:47:09 +0530

    Create dynamic textformfield and perform crud operations

---
+++ lib/main.dart
@@ -0,0 +1,307 @@
+import 'package:dynamic_textformfield/my_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:dynamic_textformfield/show_dialog.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyWidgets(),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+class _View3 extends StatefulWidget {
+  @override
+  _View3State createState() => _View3State();
+}
+
+class _View3State extends State<_View3> {
+  List<TextEditingController> _nameControllers = [];
+  List<TextField> _nameFields = [];
+  List<TextEditingController> _telControllers = [];
+  List<TextField> _telFields = [];
+  List<TextEditingController> _addressControllers = [];
+  List<TextField> _addressFields = [];
+
+  @override
+  void dispose() {
+    for (final controller in _nameControllers) {
+      controller.dispose();
+    }
+    for (final controller in _telControllers) {
+      controller.dispose();
+    }
+    for (final controller in _addressControllers) {
+      controller.dispose();
+    }
+    _okController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text("Dynamic Group Text Field"),
+          ),
+          body: Column(
+            children: [
+              Expanded(child: _listView()),
+              _addTile(),
+              _okButton(context),
+            ],
+          )),
+    );
+  }
+
+  Widget _addTile() {
+    return ListTile(
+      title: Icon(Icons.add),
+      onTap: () {
+        final name = TextEditingController();
+        final tel = TextEditingController();
+        final address = TextEditingController();
+
+        final nameField = _generateTextField(name, "name");
+        final telField = _generateTextField(tel, "mobile");
+        final addressField = _generateTextField(address, "address");
+
+        setState(() {
+          _nameControllers.add(name);
+          _telControllers.add(tel);
+          _addressControllers.add(address);
+          _nameFields.add(nameField);
+          _telFields.add(telField);
+          _addressFields.add(addressField);
+        });
+      },
+    );
+  }
+
+  TextField _generateTextField(TextEditingController controller, String hint) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: hint,
+      ),
+    );
+  }
+
+  Widget _listView() {
+    final children = [
+      for (var i = 0; i < _nameControllers.length; i++)
+        Container(
+          margin: EdgeInsets.all(5),
+          child: InputDecorator(
+            child: Column(
+              children: [
+                _nameFields[i],
+                _telFields[i],
+                _addressFields[i],
+              ],
+            ),
+            decoration: InputDecoration(
+              labelText: i.toString(),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+          ),
+        )
+    ];
+    return SingleChildScrollView(
+      child: Column(
+        children: children,
+      ),
+    );
+  }
+
+  final _okController = TextEditingController();
+  Widget _okButton(BuildContext context) {
+    final textField = TextField(
+      controller: _okController,
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+      ),
+    );
+
+    final button = ElevatedButton(
+      onPressed: () async {
+        String text = _nameControllers
+            .where((element) => element.text != "")
+            .fold("", (acc, element) => acc += "${element.text}\n");
+        text = text + _telControllers
+            .where((element) => element.text != "")
+            .fold("", (acc, element) => acc += "${element.text}\n");
+        text = text + _addressControllers
+            .where((element) => element.text != "")
+            .fold("", (acc, element) => acc += "${element.text}\n");
+      /*  final index = int.parse(_okController.text);
+        String text = "name: ${_nameControllers[index].text}\n" +
+            "tel: ${_telControllers[index].text}\n" +
+            "address: ${_addressControllers[index].text}";*/
+        await showMessage(context, text, "Result");
+      },
+      child: Text("OK"),
+    );
+
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Container(
+          child: textField,
+          width: 100,
+          height: 50,
+        ),
+        button,
+      ],
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class _View1 extends StatefulWidget {
+  @override
+  _View1State createState() => _View1State();
+}
+
+class _View1State extends State<_View1> {
+  List<TextEditingController> _controllers = [];
+  List<TextField> _fields = [];
+
+  @override
+  void dispose() {
+    for (final controller in _controllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text("Dynamic Text Field"),
+          ),
+          body: Column(
+            children: [
+              _addTile(),
+              Expanded(child: _listView()),
+              _okButton(),
+            ],
+          )),
+    );
+  }
+
+  Widget _addTile() {
+    return ListTile(
+      title: Icon(Icons.add),
+      onTap: () {
+        final controller = TextEditingController();
+        final field = TextField(
+          controller: controller,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: "name${_controllers.length + 1}",
+          ),
+        );
+
+        setState(() {
+          _controllers.add(controller);
+          _fields.add(field);
+        });
+      },
+    );
+  }
+
+  Widget _listView() {
+    return ListView.builder(
+      itemCount: _fields.length,
+      itemBuilder: (context, index) {
+        return Container(
+          margin: EdgeInsets.all(5),
+          child: _fields[index],
+        );
+      },
+    );
+  }
+
+  Widget _okButton() {
+    return ElevatedButton(
+      onPressed: () async {
+        String text = _controllers
+            .where((element) => element.text != "")
+            .fold("", (acc, element) => acc += "${element.text}\n");
+        final alert = AlertDialog(
+          title: Text("Count: ${_controllers.length}"),
+          content: Text(text),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("OK"),
+            ),
+          ],
+        );
+        await showDialog(
+          context: context,
+          builder: (BuildContext context) => alert,
+        );
+        setState(() {});
+      },
+      child: Text("OK"),
+    );
+  }
+}
---
+++ lib/my_widgets.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+class MyWidgets extends StatefulWidget {
+  const MyWidgets({Key? key}) : super(key: key);
+
+  @override
+  State<MyWidgets> createState() => _MyWidgetsState();
+}
+
+class _MyWidgetsState extends State<MyWidgets> {
+  List<TextEditingController> itemNameControllers = [];
+  List<TextFormField> itemNameFields = [];
+  List<TextEditingController> qtyControllers = [];
+  List<TextFormField> qtyFields = [];
+  List<TextEditingController> priceControllers = [];
+  List<TextFormField> priceFields = [];
+  List<TextEditingController> discControllers = [];
+  List<TextFormField> discFields = [];
+  List<String> invoiceList = [];
+  List<num> listOfPrices = [];
+  @override
+  void dispose() {
+    for (final controller in itemNameControllers) {
+      controller.dispose();
+    }
+    for (final controller in qtyControllers) {
+      controller.dispose();
+    }
+    for (final controller in priceControllers) {
+      controller.dispose();
+    }
+    for (final controller in discControllers) {
+      controller.dispose();
+    }
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(title: const Text('Dynamic TextFormFields'),),
+      body:ListView(
+        padding: const EdgeInsets.all(20),
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ..._getFriends(),
+              const SizedBox(height: 20,),
+              FloatingActionButton(
+                onPressed: (){
+                 // invoiceList.insert(invoiceList.length,"${invoiceList.length}");
+                  //setState((){});
+                  final itemName = TextEditingController();
+                  final qty = TextEditingController();
+                  final price = TextEditingController();
+                  final disc = TextEditingController();
+                  final itemField = _generateTextFieldNew(itemName, "Item");
+                  final qtyField = _generateTextField(qty, "Qty");
+                  final priceField = _generateTextField(price, "Pr.");
+                  final discField = _generateTextField(disc, "Disc(%)");
+                  setState(() {
+                    invoiceList.insert(invoiceList.length, "${invoiceList.length}");
+                    itemNameControllers.add(itemName);
+                    qtyControllers.add(qty);
+                    itemNameFields.add(itemField);
+                    qtyFields.add(qtyField);
+                    priceControllers.add(price);
+                    discControllers.add(disc);
+                    priceFields.add(priceField);
+                    discFields.add(discField);
+
+                  });
+                },child: const Icon(Icons.add),
+              ),
+              const SizedBox(height: 20,),
+              TextButton(
+                onPressed: (){
+                },
+                child: const Text('Submit')),
+              const SizedBox(height: 20,),
+           //  Text('${getTotalPrice()}'),
+              //..._getTotalAmounts(),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  num getTotalPrice(){
+  num numValue = 0;
+    for(int i=0; i<listOfPrices.length; i++){
+      numValue = listOfPrices[i] + numValue;
+    }
+    return numValue;
+  }
+  TextFormField _generateTextFieldNew(TextEditingController controller, String hint) {
+    return TextFormField(
+      keyboardType: TextInputType.text,
+      textInputAction: TextInputAction.next,
+      onChanged: (value){
+       setState((){});
+      },
+      controller: controller,
+      decoration: InputDecoration(
+        border: const UnderlineInputBorder(),
+        labelText: hint,
+      ),
+    );
+  }
+
+  TextFormField _generateTextField(TextEditingController controller, String hint) {
+    return TextFormField(
+      keyboardType: TextInputType.number,
+      textInputAction: TextInputAction.next,
+      onChanged: (value){
+        setState((){
+        });
+      },
+      controller: controller,
+      decoration: InputDecoration(
+        border: const UnderlineInputBorder(),
+        labelText: hint,
+      ),
+    );
+  }
+  List<Widget> _getFriends(){
+    List<num> getTotalAmounts = [];
+    List<Widget> friendsTextFields = [];
+    for(int i=0; i<invoiceList.length; i++){
+      num? qty = num.tryParse(qtyControllers[i].text) == null ? 0 : num.tryParse(qtyControllers[i].text);
+      num? price = num.tryParse(priceControllers[i].text) == null ? 0 : num.tryParse(priceControllers[i].text);
+      num? disc = num.tryParse(discControllers[i].text) == null ? 0 : num.tryParse(discControllers[i].text);
+      num? total = qty! * price!;
+    num? discPrice = total - (total* (disc! / 100));
+      debugPrint('Discount price is : $discPrice  & index is  :  $i');
+      getTotalAmounts.insert(i, discPrice);
+      listOfPrices=getTotalAmounts;
+      friendsTextFields.add(
+          Row(
+            children: [
+              Expanded(child: Text("$discPrice")),
+              Expanded(child: itemNameFields[i]),
+              Expanded(child: qtyFields[i]),
+              Expanded(child: priceFields[i]),
+              Expanded(child:discFields[i]),
+              _addRemoveButton(i),
+            ],
+          )
+      );
+    }
+    return friendsTextFields;
+  }
+  /// add / remove button
+  Widget _addRemoveButton(int index){
+    return InkWell(
+      onTap: (){
+        itemNameControllers.removeAt(index);
+        itemNameFields.removeAt(index);
+        invoiceList.removeAt(index);
+        qtyFields.removeAt(index);
+        qtyControllers.removeAt(index);
+        priceFields.removeAt(index);
+        priceControllers.removeAt(index);
+        discFields.removeAt(index);
+        discControllers.removeAt(index);
+        setState((){});
+      },
+      child: Container(
+        width: 30,
+        height: 30,
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: const Icon(Icons.remove, color: Colors.white,),
+      ),
+    );
+  }
+}
